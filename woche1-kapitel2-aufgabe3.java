54285f61f36f3a4a104bd4fc20e5a709d560bebb
mary <Wifi-cable@users.noreply.github.com>
2016-03-27 18:56:00 +0200

    kapitel2 aufgabe3 rechen mit java

---
+++ woche1-kapitel2-aufgabe3.java
@@ -0,0 +1,12 @@
+woche1 kapitel2 aufgabe3
+
+public class Rechnung {
+    public static void main(String[]args){
+        // Fuege hier deinen Code ein
+        int a=4;
+        int b=7;
+        int c = (a+b)*(a+b);
+        String ergebnis = "Das Ergenbins ist " +c;
+        System.out.println(ergebnis);
+    }
+}
