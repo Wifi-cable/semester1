b76e13f25c12e0cd373dd085310bb693eb1ae00c
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-03-31 12:34:57 -0700

    Papagei.java extracted
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

---
+++ Papagei.java
@@ -0,0 +1,9 @@
+public class Papagei{
+    int alter= 5;
+
+    void sagAlter() {
+        System.out.print("Hallo, ich bin ");
+        System.out.print (alter);
+        System.out.print (" jahre alt");
+    }
+}    
