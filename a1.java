df4b05ba617292009bab43238fb38c17be20fcd9
Wifi-cable <Wifi-cable@users.noreply.github.com>
2016-04-12 15:43:01 +0200

    seperate method used in main

--- a1.java
+++ a1.java
@@ -6,14 +6,21 @@ public class a1 {
 	Scanner bin = new Scanner(System.in);	 // new scanner
 	System.out.println("Bitte eine ganze zahl eingeben um sie in Binär umgewandelt zu bekommen"); //input
 	int zahl = bin.nextInt(); //uses the input, assigns varable
+	berechneBin(zahl);
+	}
+	public static void berechneBin(int dez){
+		
 	// how to make it recursive, assingning the zahl variable again and again till a zero is reached?
-	int ergebniss= (zahl/2 mod *2); //divides input by 2 and checks for a rest.
-	if (mod 1) true; // checks for rest
+	int rest= (dez %2);
+	int ergebniss = dez/2;//divides input by 2 and checks for a rest.
+	if (dez% 2==1){  // checks for rest
 		System.out.print(1); //shows a 1 if there is a rest
-		else ();
-		Sysem.out.print(0);	//shows  a zero if no rest
+	}
 	
-	System.out.println("das ist das"+ ergenniss); 
+		else{
+		System.out.print(0);	//shows  a zero if no rest
+		}
+	System.out.println("das ist das"+ ergebniss); 
 
 	}
 
