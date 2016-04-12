57af038a1b85c7756d83595d99bd8e00ed74eba7
Wifi-cable <Wifi-cable@users.noreply.github.com>
2016-04-12 15:28:01 +0200

    uebung2

---
+++ a1.java
@@ -0,0 +1,20 @@
+package uebung2;
+import java.util.Scanner; // makes scanner useable in this class
+public class a1 {
+
+	public static void main(String[] args) { // main method
+	Scanner bin = new Scanner(System.in);	 // new scanner
+	System.out.println("Bitte eine ganze zahl eingeben um sie in Binär umgewandelt zu bekommen"); //input
+	int zahl = bin.nextInt(); //uses the input, assigns varable
+	// how to make it recursive, assingning the zahl variable again and again till a zero is reached?
+	int ergebniss= (zahl/2 mod *2); //divides input by 2 and checks for a rest.
+	if (mod 1) true; // checks for rest
+		System.out.print(1); //shows a 1 if there is a rest
+		else ();
+		Sysem.out.print(0);	//shows  a zero if no rest
+	
+	System.out.println("das ist das"+ ergenniss); 
+
+	}
+
+}
