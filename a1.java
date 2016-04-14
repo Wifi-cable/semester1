ec169491629e54f91a77396b2853f1b55fc84d64
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-13 18:10:16 -0700

    a1.java : recursive solution outside uebung2 package
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- a1.java
+++ a1.java
@@ -1,27 +1,20 @@
-package uebung2;
+//package uebung2;
 import java.util.Scanner; // makes scanner useable in this class
 public class a1 {
 
 	public static void main(String[] args) { // main method
 	Scanner bin = new Scanner(System.in);	 // new scanner
-	System.out.println("Bitte eine ganze zahl eingeben um sie in Binär umgewandelt zu bekommen"); //input
+	System.out.println("Bitte eine ganze Zahl eingeben um sie in Binaer umgewandelt zu bekommen"); //input
 	int zahl = bin.nextInt(); //uses the input, assigns varable
-	berechneBin(zahl);
+	System.out.println("das Ergebnis ist "+berechneBin(zahl));
 	}
-	public static void berechneBin(int dez){
-		
-	// how to make it recursive, assingning the zahl variable again and again till a zero is reached?
-	int rest= (dez %2);
-	int ergebniss = dez/2;//divides input by 2 and checks for a rest.
-	if (dez% 2==1){  // checks for rest
-		System.out.print(1); //shows a 1 if there is a rest
+	// how to make it recursive, assingning the zahl variable again and again till a zero is reached
+	static String berechneBin(int dez){
+		String result= new String("");
+        if (dez > 0)
+        {
+            result= berechneBin(dez>>1)+(dez&1); 
+        }
+        return result;
 	}
-	
-		else{
-		System.out.print(0);	//shows  a zero if no rest
-		}
-	System.out.println("das ist das"+ ergebniss); 
-
-	}
-
 }
