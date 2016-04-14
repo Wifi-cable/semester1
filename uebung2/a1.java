7a4c113df85e09b02a4cc24924a94419db3bc8de
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-13 21:50:48 -0700

    uebung2/a1.java : replace tab by 4 spaces
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- uebung2/a1.java
+++ uebung2/a1.java
@@ -2,22 +2,25 @@ package uebung2;
 import java.util.Scanner; // makes scanner useable in this class
 public class a1 {
 
-	public static void main(String[] args) { // main method
-	Scanner bin = new Scanner(System.in);	 // new scanner
-	System.out.println("Bitte eine ganze Zahl eingeben um sie in Binaer umgewandelt zu bekommen"); //input
-	int zahl = bin.nextInt(); //uses the input, assigns varable
-	System.out.println("das Ergebnis ist binaer "+berechneBin(zahl));
-	System.out.println("der Gray code ist "+berechneGray(zahl));
-	}
-	// how to make it recursive, assingning the zahl variable again and again till a zero is reached
-	static String berechneBin(int dez){
-		String result= new String("");
-        if (dez > 0)
+    public static void main(String[] args) { // main method
+    Scanner bin = new Scanner(System.in);    // new scanner
+    System.out.println("Bitte eine ganze Zahl eingeben um sie in Binaer umgewandelt zu bekommen"); //input
+    int zahl = bin.nextInt(); //uses the input, assigns varable
+    System.out.println("das Ergebnis ist binaer "+berechneBin(zahl));
+    System.out.println("der Gray code ist "+berechneGray(zahl));
+    }
+    // how to make it recursive, assingning the zahl variable again and again till a zero is reached
+    static String dec2bin(int dez){
+        String result= new String("");
+        if (dez != 0)
         {
-            result= berechneBin(dez>>1)+(dez&1); 
+            result= dec2bin(dez>>>1)+(dez&1); 
         }
         return result;
-	}
+    }
+    static String berechneBin(int dez){
+        return dez==0? "0" : dec2bin(dez);
+    }
     static String berechneGray(int dez){
         return berechneBin(dez ^ dez>>>1);
     }
