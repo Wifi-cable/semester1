f02e0ce2fcaeb0efb24d8db9db456c00faf99694
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-19 16:04:23 -0700

    src/uebung2/Int2Digits.java : add digitArray2Gray() method
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- src/uebung2/Int2Digits.java
+++ src/uebung2/Int2Digits.java
@@ -4,15 +4,17 @@ import java.util.Arrays;
 
 public class Int2Digits
 {
-    static String digits= new String("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    int base;
+    static String digits= new String("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");   // shared by all objects of class Int2Digits
+    int base;   // number base to convert to, each object has its own
 
-    public Int2Digits(int base)   // constructor with base parameter
+    public Int2Digits(int base)
+    // constructor with base parameter
     {
         this.base=base;
     }
 
-    public int getBase()    // base getter function
+    public int getBase()
+    // base getter function
     {
         return base;
     }
@@ -20,7 +22,7 @@ public class Int2Digits
     public int[] int2digitArray(int number)
     // convert number to LITTLE ENDIAN integer array
     {
-        int[] digitArray= new int[32];  // maximum number of digits
+        int[] digitArray= new int[32];  // allocate array for maximum number of digits
         int digit= 0;
         do  // run at least once even if number == 0
         {
@@ -28,7 +30,22 @@ public class Int2Digits
             number /= base;
         }
         while (number > 0);
-        return Arrays.copyOf(digitArray, digit);
+        return Arrays.copyOf(digitArray, digit);    // return an array with only the number of digits used
+    }
+
+    public int[] digitArray2Gray(int[] digitArray)
+    // convert LITTLE ENDIAN integer array to LITTLE ENDIAN Gray code array
+    // lifted from https://en.wikipedia.org/wiki/Gray_code#n-ary_Gray_code
+    {
+        int digit= digitArray.length;
+        int[] grayArray= new int[digit];
+        int shift= 0;
+        while (digit-- > 0)
+        {
+            grayArray[digit]= (digitArray[digit] + shift) % base;
+            shift+= base-grayArray[digit];
+        }
+        return grayArray;
     }
 
     public String digitArray2string(int[] digitArray)
