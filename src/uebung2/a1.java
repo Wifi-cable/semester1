b7fdcf5e5c6e6c2c952b8af174d5defe3d592881
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-19 16:07:17 -0700

    uebung2/a1.java : use Gray code converter and add some bases for good measure
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- src/uebung2/a1.java
+++ src/uebung2/a1.java
@@ -7,16 +7,19 @@ public class a1 {
         Scanner bin = new Scanner(System.in);    // new scanner
         System.out.println("Bitte eine ganze Zahl eingeben um sie in Binaer umgewandelt zu bekommen"); //input
         int zahl = bin.nextInt(); //uses the input, assigns varable
-        System.out.println("das Ergebnis ist binaer "+berechneBin(zahl));
+        System.out.println("das Ergebnis ist binaer   "+berechneBin(zahl));
         System.out.println("der binaere Gray code ist "+berechneGray(zahl));
         System.out.println("das Ergebnis ist hexadezimal "+berechneHex(zahl));
 
         System.out.println("And now ...\nfor Something Completely Different");
         Int2Digits[] converters = new Int2Digits[] {
                 new Int2Digits(2),
+                new Int2Digits(4),
                 new Int2Digits(5),
                 new Int2Digits(8),
-                new Int2Digits(16)
+                new Int2Digits(10),
+                new Int2Digits(16),
+                new Int2Digits(36)
         };
         for (Int2Digits converter : converters )
         {
@@ -26,6 +29,12 @@ public class a1 {
                 converter.getBase()+
                 " is "+
                 converter.int2string(zahl));
+            System.out.println(" "+
+                zahl+ 
+                " in base "+
+                converter.getBase()+
+                " Gray code is "+
+                converter.digitArray2string(converter.digitArray2Gray(converter.int2digitArray(zahl))));
         }
     }
     // how to make it recursive, assingning the zahl variable again and again till a zero is reached
