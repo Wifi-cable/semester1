9cfe29cd2a4e470831e5267a053008decc6e19f3
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-19 14:44:16 -0700

    uebung2/a1.java : also use Int2Digits() class
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- src/uebung2/a1.java
+++ src/uebung2/a1.java
@@ -4,15 +4,33 @@ public class a1 {
     static String digits= new String("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
     public static void main(String[] args) { // main method
-    Scanner bin = new Scanner(System.in);    // new scanner
-    System.out.println("Bitte eine ganze Zahl eingeben um sie in Binaer umgewandelt zu bekommen"); //input
-    int zahl = bin.nextInt(); //uses the input, assigns varable
-    System.out.println("das Ergebnis ist binaer "+berechneBin(zahl));
-    System.out.println("der binaere Gray code ist "+berechneGray(zahl));
-    System.out.println("das Ergebnis ist hexadezimal "+berechneHex(zahl));
+        Scanner bin = new Scanner(System.in);    // new scanner
+        System.out.println("Bitte eine ganze Zahl eingeben um sie in Binaer umgewandelt zu bekommen"); //input
+        int zahl = bin.nextInt(); //uses the input, assigns varable
+        System.out.println("das Ergebnis ist binaer "+berechneBin(zahl));
+        System.out.println("der binaere Gray code ist "+berechneGray(zahl));
+        System.out.println("das Ergebnis ist hexadezimal "+berechneHex(zahl));
+
+        System.out.println("And now ...\nfor Something Completely Different");
+        Int2Digits[] converters = new Int2Digits[] {
+                new Int2Digits(2),
+                new Int2Digits(5),
+                new Int2Digits(8),
+                new Int2Digits(16)
+        };
+        for (Int2Digits converter : converters )
+        {
+            System.out.println("The number "+
+                zahl+
+                " in base "+
+                converter.getBase()+
+                " is "+
+                converter.int2string(zahl));
+        }
     }
     // how to make it recursive, assingning the zahl variable again and again till a zero is reached
-    static String dec2bin(int dez){
+    static String dec2bin(int dez)
+    {
         String result= new String("");
         if (dez != 0)
         {
@@ -20,10 +38,12 @@ public class a1 {
         }
         return result;
     }
-    static String berechneBin(int dez){
+    static String berechneBin(int dez)
+    {
         return dez==0? "0" : dec2bin(dez);
     }
-    static String dec2hex(int dez){
+    static String dec2hex(int dez)
+    {
         String result= new String("");
         if (dez != 0)
         {
@@ -31,10 +51,12 @@ public class a1 {
         }
         return result;
     }
-    static String berechneHex(int dez){
+    static String berechneHex(int dez)
+    {
         return dez==0? "0" : dec2hex(dez);
     }
-    static String berechneGray(int dez){
+    static String berechneGray(int dez)
+    {
         return berechneBin(dez ^ dez>>>1);
     }
 }
