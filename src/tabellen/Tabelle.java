f236ba79153f1dfccf4233cb23142c80e441a5be
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-29 12:27:24 -0700

    src/tabellen/Tabelle.java : javadoc compatible comment
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- src/tabellen/Tabelle.java
+++ src/tabellen/Tabelle.java
@@ -1,10 +1,21 @@
 package tabellen;
 
+/**
+ * Eine Klasse, die Tabellenausdruck-Methoden beinhaltet
+ */
 public class Tabelle {
-    void print(){
-        for (int t=0;t<9;t++){
+    /**
+     * Methode, um Funktionen auszudrucken
+     * 
+     * Bisher kein Parameter, deshalb funktioniert diese Methode bisher nicht
+     */
+    public void print()
+    {
+        for (int t=0;t<9;t++)
+        {
             System.out.print(board[t] + "|"); 
-            if (t==2 || t==5){
+            if (t==2 || t==5)
+            {
                 System.out.print("\n------\n");
             } 
         } 
